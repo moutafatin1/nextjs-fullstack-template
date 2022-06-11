23424e52b1802087c6e9166c29b4501be36cf2ec
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-11 16:47:38 +0100

    fix: configure _app.tsx using withTRPC higher-order component, and export getBaseUrl function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import { AppRouter } from '@/server/routers';
+import { getBaseUrl } from '@/utils/getBaseUrl';
+import { withTRPC } from '@trpc/next';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
@@ -10,4 +13,11 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   );
 }
 
-export default MyApp;
+export default withTRPC<AppRouter>({
+  config() {
+    const url = `${getBaseUrl()}/api/trpc`;
+
+    return { url };
+  },
+  ssr: false,
+})(MyApp);
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -2,3 +2,4 @@ import { AppRouter } from '@/server/routers';
 import { createReactQueryHooks } from '@trpc/react';
 
 export const trpc = createReactQueryHooks<AppRouter>();
+
